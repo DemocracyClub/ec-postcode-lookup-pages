47d5cff20e5327e72107a7ec04967aaa292b048c
Sym Roe <sym.roe@talusdesign.co.uk>
2023-02-13 16:44:34 +0000

    Install pydantic when installing package

--- lib/response_builder/setup.py
+++ lib/response_builder/setup.py
@@ -12,5 +12,5 @@ setup(
     packages=["response_builder"],
     package_dir={"response_builder": "."},
     package_data={"response_builder": ["v1/*", "v1/**/*"]},
-    install_requires=["uk-election-ids==0.5.1"],
+    install_requires=["uk-election-ids==0.5.1", "pydantic[email]>=1.10,<2"],
 )
