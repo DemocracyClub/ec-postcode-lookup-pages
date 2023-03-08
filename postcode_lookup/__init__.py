bb8eb12c7e120d2d035f0da8dd4a5ae1d4215e9a
Sym Roe <sym.roe@talusdesign.co.uk>
2023-03-08 16:11:38 +0000

    Comment about import paths

--- postcode_lookup/__init__.py
+++ postcode_lookup/__init__.py
@@ -1,5 +1,11 @@
 import sys
 from pathlib import Path
 
+
+# This is needed to make imports work on AWS Lambda.
+# When running there, the `postcode_lookup` directory is the root
+# meaning that we can't use that in our imports.
+# When running locally, we have to add the `postcode_lookup` directory
+# to the path
 source_route = Path(__file__).parent.absolute()
 sys.path.append(str(source_route))
