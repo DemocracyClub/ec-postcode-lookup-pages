0f077572307ceb041acf7d5edc4af05e079b66d7
Sym Roe <sym.roe@talusdesign.co.uk>
2023-03-08 16:55:10 +0000

    Wait longer after deploy

--- .circleci/tests/test_against_fqdn.py
+++ .circleci/tests/test_against_fqdn.py
@@ -9,6 +9,12 @@ FAILOVER_COMMENT = "DC failover page"
 STATIC_PAGE_COMMENT = "<!-- Static page content -->"
 
 
+def make_request(url):
+    req = httpx.get(url, timeout=20)
+    req.raise_for_status()
+    return req
+
+
 @pytest.fixture(scope="session")
 def fqdn():
     if FQDN := os.environ.get("FQDN"):
@@ -21,36 +27,31 @@ def test_sandbox_responses(fqdn, postcode):
     if "electoralcommission.org.uk" in fqdn:
         return
     url = f"https://{fqdn}/sandbox/polling-stations?postcode-search={postcode}"
-    req = httpx.get(url)
-    req.raise_for_status()
+    req = make_request(url)
     assert FAILOVER_COMMENT not in req.text
 
 
 def test_failover_page(fqdn):
     url = f"https://{fqdn}/polling-stations?postcode-search=FA1LL&Submit+Postcode="
-    req = httpx.get(url)
-    req.raise_for_status()
+    req = make_request(url)
     assert FAILOVER_COMMENT in req.text
 
 
 def test_smoke_test_live_api(fqdn):
     url = f"https://{fqdn}/polling-stations?postcode-search=SW1A1AA&Submit+Postcode="
-    req = httpx.get(url)
-    req.raise_for_status()
+    req = make_request(url)
     assert FAILOVER_COMMENT not in req.text
 
 
 def test_postcode_form_en(fqdn):
     url = f"https://{fqdn}/i-am-a/voter/your-election-information"
-    req = httpx.get(url)
-    req.raise_for_status()
+    req = make_request(url)
     assert STATIC_PAGE_COMMENT in req.text
     assert FAILOVER_COMMENT not in req.text
 
 
 def test_postcode_form_cy(fqdn):
     url = f"https://{fqdn}/cy/rwyf-yneg-pleidleisiwr/pleidleisiwr/gwybodaeth-etholiad"
-    req = httpx.get(url)
-    req.raise_for_status()
+    req = make_request(url)
     assert STATIC_PAGE_COMMENT in req.text
     assert FAILOVER_COMMENT not in req.text
