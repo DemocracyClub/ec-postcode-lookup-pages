843fb87ed5ed7ca10632e6f86bf127a3c1319f8e
Virginia Dooley <vcdooley@gmail.com>
2024-02-27 13:47:06 +0000

    No local ballots

--- postcode_lookup/mock_responses.py
+++ postcode_lookup/mock_responses.py
@@ -1,4 +1,5 @@
 from response_builder.v1.generated_responses.root_responses import (
+    NO_LOCAL_BALLOTS,
     SINGLE_LOCAL_FUTURE_BALLOT_WITH_POLLING_STATION,
 )
 
@@ -8,5 +9,8 @@ example_responses = {
     "MM1 1AA": {
         "description": "Single local ballot",
         "response": SINGLE_LOCAL_FUTURE_BALLOT_WITH_POLLING_STATION,
-    }
+    "MM1 1AC": {
+        "description": "No local ballots",
+        "response": NO_LOCAL_BALLOTS,
+    },
 }
