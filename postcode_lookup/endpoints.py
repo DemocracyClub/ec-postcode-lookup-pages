23b4de7c2ecd8011b91eb7ba0c06bcca5558ebe5
Sym Roe <sym.roe@talusdesign.co.uk>
2024-03-25 12:03:04 +0000

    Allow testing single sections on one page

--- postcode_lookup/endpoints.py
+++ postcode_lookup/endpoints.py
@@ -11,8 +11,11 @@ from dc_api_client import (
     MockAPIBackend,
     SandboxAPIBackend,
 )
+from markupsafe import Markup
 from mock_responses import example_responses
-from response_builder.v1.models.base import RootModel
+from response_builder.v1.builders.ballots import StockLocalBallotBuilder
+from response_builder.v1.generated_responses.candidates import all_candidates
+from response_builder.v1.models.base import CancellationReason, RootModel
 from response_builder.v1.sandbox import SANDBOX_POSTCODES
 from starlette.requests import Request
 from starlette.responses import RedirectResponse, Response
@@ -168,6 +171,14 @@ def results_context(api_response):
     }
 
 
+def get_ballot_stages(poll_open_date):
+    return {
+        "Polling day": poll_open_date,
+        "After SOPNs": poll_open_date + datetime.timedelta(days=20),
+        "Before SOPNs": poll_open_date + datetime.timedelta(days=35),
+    }
+
+
 async def redirect_root_to_postcode_form(request: Request):
     """
     We don't respond to "/" on the live site, but when in dev mode it's handy
@@ -182,11 +193,7 @@ async def redirect_root_to_postcode_form(request: Request):
         return Response(status_code=404)
 
     poll_open_date = datetime.date.today()
-    ballot_stages = {
-        "Polling day": poll_open_date,
-        "After SOPNs": poll_open_date + datetime.timedelta(days=20),
-        "Before SOPNs": poll_open_date + datetime.timedelta(days=35),
-    }
+    ballot_stages = get_ballot_stages(poll_open_date)
 
     return get_loader(request).TemplateResponse(
         "debug_page.html",
@@ -201,3 +208,41 @@ async def redirect_root_to_postcode_form(request: Request):
 
 def failover(request: Request):
     return Response(status_code=400)
+
+
+async def section_tester(request: Request):
+    template_name = "section_tester.html"
+
+    sections = []
+
+    if request.path_params["section"] == "cancellation_reasons":
+        base_ballot = StockLocalBallotBuilder().with_candidates(all_candidates)
+        for cancellation_reason in CancellationReason:
+            ballot = base_ballot.with_cancellation_reason(
+                cancellation_reason
+            ).build()
+            template = get_loader(request).TemplateResponse(
+                "includes/cancellation_reasons.html",
+                {
+                    "request": request,
+                    "initial_poll_date": ballot.poll_open_date,
+                    "ballot": ballot,
+                },
+            )
+
+            sections.append(
+                {
+                    "content": Markup(template.body.decode()).replace(
+                        "\\n", ""
+                    ),
+                    "title": cancellation_reason.name,
+                }
+            )
+
+    return get_loader(request).TemplateResponse(
+        template_name,
+        {
+            "request": request,
+            "sections": sections,
+        },
+    )
