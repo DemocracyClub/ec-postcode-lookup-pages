9b707716862a8a0b0769db1fff8718c651818882
chris48s <git@chris-shaw.dev>
2026-09-23 13:45:15 +0100

    fix comment

--- postcode_lookup/scheduled_elections.py
+++ postcode_lookup/scheduled_elections.py
@@ -18,7 +18,7 @@ Note: It might be tempting to add "London" in here but
     - Newham
     - Tower Hamlets
 ..which is enough edge cases that it is
-not sensible to try and generalse about London here.
+not sensible to try and generalise about London here.
 """
 KNOWN_SCHEDULED_ELECTIONS = {
     Country.SCOTLAND: [
